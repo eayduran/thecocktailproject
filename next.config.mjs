6cfca72a9d8efd99f2b6de7e3d6ff9616fd132c2
Enes Ayduran <ayduranenes@gmail.com>
2024-08-10 13:39:23 +0300

    feat: add next image config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+  reactStrictMode: false,
+  images: {
+    domains: ["www.thecocktaildb.com"],
+  },
+};
 
 export default nextConfig;
